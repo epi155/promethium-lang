4b37f6795e7054c7588dbb8854b1152301b62b86
enrico <e.pistolesi@gmail.com>
2023-05-09 19:17:50 +0200

    restore module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,7 @@
-import io.github.epi155.pm.lang.ReportPrintStreamProvider;
-
 /**
  * Utility module for handling errors xor a value
  */
 module promethium.lang {
-    uses ReportPrintStreamProvider;
     exports io.github.epi155.pm.lang;
 
     requires org.jetbrains.annotations;
